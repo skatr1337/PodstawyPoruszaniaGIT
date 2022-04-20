f5aae1e49422ca56328d9add514ca6777a713504
Filip <filip.biegaj@protonmail.com>
2022-04-20 19:43:54 +0200

    lab3

---
+++ plik.sh
@@ -0,0 +1,58 @@
+git commit
+git commit
+
+git branch bugFix
+git checkout bugFix
+git checkout -b bugFix
+
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+git checkout C4
+
+git checkout C4
+git checkout HEAD^
+
+git branch -f main C6
+git branch -f bugFix C0
+git checkout HEAD^
+
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7
+
+git rebase -i HEAD~4
+
+git checkout main
+git cherry-pick C4
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git rebase caption main
+
+git checkout main
+git cherry-pick C2 C3
+
+git tag v0 C1
+git tag v1 C2
+git checkout HEAD^
+
+git describe main
+git describe side
+git describe bugFix
+git commit
